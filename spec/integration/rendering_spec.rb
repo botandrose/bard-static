b9d7aba25c5d322612cb250eee3811287240af68
Micah Geisel <micah@botandrose.com>
2011-11-21 13:22:22 -0800

    add some tests for static.

--- spec/integration/rendering_spec.rb
+++ spec/integration/rendering_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "Rendering" do
+describe "Rendering mockups" do
   it "renders index.html.erb at /mockups" do
     get "/mockups"
     response.body.should include("INDEX FILE")
@@ -21,3 +21,30 @@ describe "Rendering" do
     response.body.should include("FORM PARTIAL")
   end
 end
+
+describe "Rendering static" do
+  it "renders index.html.erb at / with layout" do
+    get "/"
+    response.body.should include("LAYOUT")
+    response.body.should include("STATIC INDEX FILE")
+  end
+
+  it "renders sites.html.erb at /sites with layout" do
+    get "/sites"
+    response.body.should include("LAYOUT")
+    response.body.should include("STATIC SITES FILE")
+  end
+
+  it "renders posts/new.html.erb at /posts/new with layout" do
+    get "/posts/new"
+    response.body.should include("LAYOUT")
+    response.body.should include("NEW POST FILE")
+  end
+
+  it "renders partials with layout" do
+    get "/posts/new"
+    response.body.should include("LAYOUT")
+    response.body.should include("FORM PARTIAL")
+  end
+end
+
