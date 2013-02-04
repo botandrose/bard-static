369c1bad2c34a534a7f4bc82eb05c19acd3340cd
Micah Geisel <micah@botandrose.com>
2013-02-04 10:42:19 -0800

    only route get requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace "mockups", :module => :bard_static do
     root :to => "static#mockups", :file_path => "index"
-    match "/*file_path" => "static#mockups"
+    get "/*file_path" => "static#mockups"
   end
   root :to => "bard_static/static#static", :file_path => "index"
-  match "*file_path" => "bard_static/static#static"
+  get "*file_path" => "bard_static/static#static"
 end
