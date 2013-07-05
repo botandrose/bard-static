cc2f799897d70660adce5511ff75c8b3644e6fee
Micah Geisel <micah@botandrose.com>
2013-07-05 12:06:56 -0700

    ruby 1.8 compat.

--- app/helpers/bard_static/layout_helper.rb
+++ app/helpers/bard_static/layout_helper.rb
@@ -1,7 +1,7 @@
 module BardStatic
   module LayoutHelper
     def layout layout, &block
-      render text: capture(&block), layout: "layouts/#{layout}"
+      render :text => capture(&block), :layout => "layouts/#{layout}"
     end
   end
 end
