8a9d2c5013f4f4485a80c8117eeca7dce6b6aa63
Micah Geisel <micah@botandrose.com>
2017-02-17 15:51:34 -0800

    add #mockup_form_for.

--- app/helpers/bard/static/layout_helper.rb
+++
@@ -1,9 +0,0 @@
-module Bard
-  module Static
-    module LayoutHelper
-      def layout layout, &block
-        render :text => capture(&block), :layout => "layouts/#{layout}"
-      end
-    end
-  end
-end
---
+++ app/helpers/bard/static/mockup_helper.rb
@@ -0,0 +1,19 @@
+require "ostruct"
+
+module Bard
+  module Static
+    module MockupHelper
+      def layout layout, &block
+        render :text => capture(&block), :layout => "layouts/#{layout}"
+      end
+
+      def mockup_form_for &block
+        form_for MockupModel.new, url: ".", &block
+      end
+
+      class MockupModel < OpenStruct
+        include ActiveModel::Model
+      end
+    end
+  end
+end
