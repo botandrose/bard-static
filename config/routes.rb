9580e83478f0dd8bd3bfccf01152ea0bc817d5bf
Micah Geisel <micah@botandrose.com>
2023-12-02 11:07:25 -0600

    define root_path.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     root to: "static#mockups", file_path: "index"
     get "/*file_path" => "static#mockups"
   end
-  root to: "bard/static/static#static", file_path: "index", as: "bard_static_default_root"
+  root to: "bard/static/static#static", file_path: "index"
   get "*file_path" => "bard/static/static#static", constraints: ->(request){
     lookup_context = ActionView::LookupContext.new([Rails.root.join("app/views/static")])
     lookup_context.exists?(request.path) || lookup_context.exists?(request.path + "/index")
